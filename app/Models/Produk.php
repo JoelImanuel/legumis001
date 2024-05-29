cdf20fc8325914c28de1530b16593042e4bbb125
JoelImanuel <joelimanuel83@gmail.com>
2024-05-29 09:47:13 +0900

    penambahan blade

--- app/Models/Produk.php
+++ app/Models/Produk.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Produk extends Model
 {
     use HasFactory;
+
+    public $table = 'produk';
+
+    const CREATED_AT = 'created_at';
+    const CREATED_AT = 'updated_at';
+
+    protected $auditTimestamps = true;
+
+    public $fillable = [
+        'id',
+        'code',
+    ];
+    }
 }
